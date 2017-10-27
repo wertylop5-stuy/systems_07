1af7599cfa8049dc377ba85ba09ced8804d28e3d
sabrina-wen <sabrinawn253@gmail.com>
2017-10-27 12:11:43 -0400

    working on randgen

--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 #include<sys/stat.h>
 
 int rand_gen() {
-	
+	char rand_num[];
+	read("/dev/random", (int)rand_num, sizeof(rand_num));
+	return rand_num;
 }
 
 int main() {
-	
-	
+	rand_gen();
+
 	return 0;
 }
