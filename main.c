de9522a8c21aacf81de332132250e0b49b19df2f
Stanley Lin <slin55055@gmail.com>
2017-10-27 12:05:26 -0400

    work

--- main.c
+++ main.c
@@ -10,7 +10,27 @@ int rand_gen() {
 }
 
 int main() {
+	umask(0000);
 	
+	int arr[10];
+	int x;
+	
+	printf("generating numbers\n");
+	for (x = 0; x < 10; x++) {
+		arr[x] = rand_gen();
+		printf("%d: %d\n", x, ar[x]");
+	}
+	
+	fd = open("arr_data", O_CREATE | O_RDWR, 0644);
+	write(fd, arr, sizeof(arr));
+	
+	int copy[10];
+	read(fd, copy, sizeof(copy));
+	
+	printf("reading numbers\n");
+	for (x = 0; x < 10; x++) {
+		printf("%d: %d\n", x, copy[x]");
+	}
 	
 	return 0;
 }
